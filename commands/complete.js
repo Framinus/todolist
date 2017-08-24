fb5f2ebb573652381de7297315132bcebc7bdabd
James McCormack <thejamesmccormack@gmail.com>
2017-08-24 12:51:29 -0700

    fixed complete and delete files

--- commands/complete.js
+++ commands/complete.js
@@ -4,13 +4,15 @@ const writeFile = require('./../writefile.js');
 function completeTask(taskID) {
   const taskObj = readFile();
   let index = 0;
-  for (let i = 0; i < taskObj.tasks.length; i += 1) {
-    if (taskObj.tasks[i].id === taskID) {
-      index = taskObj.tasks[i].id;
+  let i = 0;
+  while (i < taskObj.tasks.length) {
+    if (taskObj.tasks[i].id != taskID) {
+      i += 1;
+    } else {
+      index = i;
+      i += 1;
     }
   }
-  // const index = taskID - 1;
-  // displays the deleted task ID and task text to the console.
   console.log(`Completed task ${taskID}: ${taskObj.tasks[index].task}`);
   taskObj.tasks.splice(index, 1);
   writeFile(taskObj);
--- commands/delete.js
+++ commands/delete.js
@@ -4,15 +4,16 @@ const writeFile = require('./../writefile.js');
 function deleteTask(taskID) {
   const taskObj = readFile();
   let index = 0;
-  for (let i = 0; i < taskObj.tasks.length; i += 1) {
-    console.log(taskObj.tasks[0].id);
-    if (taskObj.tasks[i].id === taskID) {
-      index = taskObj.tasks[i].id;
+  let i = 0;
+  while (i < taskObj.tasks.length) {
+    if (taskObj.tasks[i].id != taskID) {
+      i += 1;
+    } else {
+      index = i;
+      i += 1;
     }
-    return index;
   }
-  console.log(deleteTask());
-  console.log(taskObj.tasks[0].task);
+
   console.log(`Deleted task ${taskID}: ${taskObj.tasks[index].task}`);
   taskObj.tasks.splice(index, 1);
   writeFile(taskObj);
