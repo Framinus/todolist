a6d63cfa5cb57b9cd7e525630affcddf4d633f26
James McCormack <thejamesmccormack@gmail.com>
2017-09-13 15:42:11 -0700

    'made minor tweak to add.js'

--- commands/add.js
+++ commands/add.js
@@ -1,10 +1,9 @@
 const add = function (data, arg) {
-  // i'm just adding an object to the task array. done.
-  const task =
-    {
-      id: data.length + 1,
-      description: arg,
-    };
+  const task = {
+    id: data.length + 1,
+    description: arg,
+    complete: false,
+  };
   data.push(task);
   return data;
 };
