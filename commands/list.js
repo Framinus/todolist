69d55107a031f8c66c42f870cc8fb8910a120201
James McCormack <thejamesmccormack@gmail.com>
2017-09-13 18:21:21 -0700

    fixed list so it only shows uncompleted tasks

--- commands/list.js
+++ commands/list.js
@@ -2,7 +2,9 @@ const list = function (data) {
   console.log(`\nID  Description`);
   console.log(`----------------`);
   data.forEach((task) => {
-    console.log(`${task.id}   ${task.description}`);
+    if (!task.complete) {
+      console.log(`${task.id}   ${task.description}`);
+    }
   });
   console.log(`\nYou have ${data.length} tasks\n`);
   return data;
